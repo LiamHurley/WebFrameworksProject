c7ab9118c0943917079887f380dc6c8c6caa3e44
Liam <Liam.Hurley2@students.ittralee.ie>
2021-10-11 19:25:36 +0100

    Sixth commit. Data moved back to controllers and fed out onto pages. Still need to do this for 'About' page as well add some placeholder news items.

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,8 +1,38 @@
 /* GET 'home' page */
 const homelist = function(req, res){
-    res.render('index');
+    res.render('index', {
+        title: 'Destination Finder',
+        
+        pageHeader: { 
+            title: 'Destination Finder', 
+            strapline: 'Find your next holiday destination!' 
+        }, 
+    
+        locations: [{
+            name: 'Bled, Slovenia',
+            rating: 5,
+            image: '/images/lakebled.jpg',
+            alt: 'Lake Bled',
+            bio: 'lake bled, bled castle, pletnas.. etc',
+            features: ['Alpine Views','Hiking','Easy-Going']
+        },{
+            name: 'Barcelona, Spain',
+            rating: 5,
+            image: '/images/barcelona.jpg',
+            alt: 'Barcelona',
+            bio: 'catalonia.. architecture.. etc',
+            features: ['Great Food','Architecture','Churches']
+        },{
+            name: 'Paris, France',
+            rating: 2,
+            image: '/images/paris.jpg',
+            alt: 'Eiffel Tower',
+            bio: 'eiffel tower.. romantic.. etc',
+            features: ['Great For Couples','City Life','History']
+        }]
+    });
 };
 
 module.exports = {
-homelist
+    homelist
 };    
--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -1,13 +1,33 @@
 const about = function(req, res){
-    res.render('about', { title: 'About' });
+    res.render('about', { 
+        title: 'About' 
+    });
 };
 
 const register = function(req,res){
-    res.render('register');
+    res.render('register', {
+        title: 'Register',
+        
+        formLabels: {
+            formTitle: 'Create An Account',
+            nameLabel: 'Your Name',
+            emailLabel: 'Your Email',
+            passwordLabel: 'Password',
+            repeatPasswordLabel: 'Repeat your password',
+        }
+    });
 }
 
 const login = function(req,res){
-  res.render('login');
+    res.render('login', {
+        title: 'Login',
+  
+        formLabels: {
+          formTitle: 'Login',
+          emailLabel: 'Your Email',
+          passwordLabel: 'Password'
+        }
+    });
 }
   
 module.exports = {
